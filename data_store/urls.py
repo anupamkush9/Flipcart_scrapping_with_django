b0bb6956085ab35daf16058f99fe727dd58c87c5
Anupamkush9 <anupam.kush9@gmail.com>
2024-06-17 01:21:41 +0530

    Dynamically-displaying-validation-error-message-on-ui-after-form-submission

--- data_store/urls.py
+++ data_store/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.search_key, name='search'),
+    path("post_search_query/", views.PostSearchQuery.as_view(), name='post-search-query'),
     path("show-data", views.show_data, name='show-data'),
     path("show-individual/<int:id>",views.show_individual,name='show-individual')
 ]
--- data_store/views.py
+++ data_store/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from .scrapper_file import scrapper
 from .models import Datastore
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views import View
 
 @login_required
 def search_key(request):
@@ -39,3 +41,16 @@ def show_individual(request, id):
         "data_store" : datastore.raw_data,
         "product_name" : datastore.search_query
     })
+    
+    
+class PostSearchQuery(View):
+    def post(self, request, *args, **kwargs):
+        search_query = request.POST.get('search_query')
+        errors_dict = {}
+        if search_query == "credit_error":
+            errors_dict["errors"] = ["Credit limit reached! please contact the support"]
+            return JsonResponse(errors_dict)
+        if  search_query == "field_error":
+            errors_dict.update(errors = [{"code": "search_query", "message" : "Please enter valid query"}])
+        return JsonResponse(errors_dict)
+        
