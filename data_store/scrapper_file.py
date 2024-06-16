594a79e73907df547de6f89ccd0e3af0ed9288e1
Anupamkush9 <anupam.kush9@gmail.com>
2024-06-17 02:31:55 +0530

    fixed the crawler as per source change

--- data_store/scrapper_file.py
+++ data_store/scrapper_file.py
@@ -5,6 +5,8 @@ import requests
 from celery import shared_task
 from authentication.models import User
 from .models import Datastore
+import traceback
+
 @shared_task
 def scrapper(seach_key, id):
     print(seach_key, id)
@@ -22,58 +24,67 @@ def scrapper(seach_key, id):
             soup = BeautifulSoup(request.content, "html.parser")
             return soup
         except Exception as e:
-            print(e)
+            print("Error in setsoup")
+            print(traceback.format_exc())
             exit()
 
     def getdata(soup):
-        
-        title = soup.find('title') 
-        
-        if "503" in title.text:
-            print(title.text)
-            exit()
-        time.sleep(3)
+        try:
+            title = soup.find('title') 
             
-        for data in soup.findAll('div', class_="_1YokD2 _3Mn1Gg"):
-            product_name = data.findAll('div', attrs={'class' : '_4rR01T'})
-            product_price = data.findAll('div', attrs={'class' : '_30jeq3 _1_WHN1'})
-            for names in product_name:
-                item_name.append(names.text)
-            time.sleep(2)
-            for price in product_price:
-                item_price.append(price.text)
-            
-            time.sleep(2)
-            # next_url = data.find('a', attrs={"class" : "_1LKTO3"}) #_1LKTO3
-            nav = data.find('nav', class_="yFHi8N")
-            # print(nav.prettify())
-            next_url = nav.find('a', class_='_1LKTO3')
-            # breakpoint()
-            if "Next" in next_url.find('span').text:
-                # breakpoint()
-                print(next_url['href'])
-                # breakpoint()
-                time.sleep(2)
-                getdata(setsoup(next_url['href']))   
-            
-            else:
-                # print("Data Extracted Succesfully")
+            if "503" in title.text:
+                print(title.text)
+                exit()
+            time.sleep(3)
                 
-                for names in item_name:
-                    received_data={}
-                    received_data["name"] = names
-                    for prices in item_price:
-                        received_data["price"] = prices
-                        item_price.remove(prices)
-                        stored_data.append(received_data)
-                        break
-                print("=========", stored_data)
-      
-                # return stored_data
-    while 1:
+            for data in soup.findAll('div', class_="cPHDOP col-12-12"):
+                product_name = data.findAll('div', attrs={'class' : 'KzDlHZ'})
+                product_price = data.findAll('div', attrs={'class' : 'Nx9bqj _4b5DiR'})
+                for names in product_name:
+                    item_name.append(names.text)
+                for price in product_price:
+                    item_price.append(price.text)
+                
+                # time.sleep(2)
+                try:
+                    # # next_url = data.find('a', attrs={"class" : "_1LKTO3"}) #_1LKTO3
+                    # nav = data.find('nav', class_="WSL9JP")
+                    # # print(nav.prettify())
+                    # next_url = nav.find('a', class_='cn++Ap')
+                    # # breakpoint()
+                    # if "Next" in next_url.find('span').text:
+                    #     # breakpoint()
+                    #     print(next_url['href'])
+                    #     # breakpoint()
+                    #     time.sleep(2)
+                    #     getdata(setsoup(next_url['href']))   
+                    
+                    # else:
+                        # print("Data Extracted Succesfully")
+                        
+                        for names in item_name:
+                            received_data={}
+                            received_data["name"] = names
+                            for prices in item_price:
+                                received_data["price"] = prices
+                                item_price.remove(prices)
+                                stored_data.append(received_data)
+                                break
+                        print("=========", stored_data)
+                except:
+                    pass
         
-        getdata(setsoup(base_url))
-        break
+        except Exception as e:
+            print("Error in setsoup",e)
+            print(traceback.format_exc())
+
+    while 1:
+        try:
+            getdata(setsoup(base_url))
+            break
+        except Exception as e:
+            print("Error in setsoup while calling",e)
+            print(traceback.format_exc())
     data_store = Datastore.objects.filter(id=id).first()
     data_store.raw_data = stored_data
     data_store.status = "Completed"
